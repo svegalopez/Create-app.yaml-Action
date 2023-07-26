4554ddad984d174a7d6dd1ae5d69bee560aeb01e
Sebastian Vega <svegalopez@gmail.com>
2023-07-26 09:11:32 -0400

    test

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# create-app-yaml-action
-Use this action to generate an app.yaml file from your github secrets
+# Create app.yaml file Action
+
+This action creates an app.yaml file to be used with Google App Engine.
+It creates the file and injects env variables from github secrets and github variables.
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+```yaml
+uses: actions/hello-world-javascript-action@e76147da8e5c81eaf017dede5645551d4b94427b
+with:
+  who-to-greet: "Mona the Octocat"
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+const core = require("@actions/core");
+const yaml = require("js-yaml");
+
+async function run() {
+  try {
+    let data = {
+      runtime: "",
+      env_variables: {},
+    };
+
+    let outFile = "";
+    data.runtime = core.getInput("runtime") || "nodejs18";
+    const envKeys = Object.keys(process.env);
+
+    for (const key of envKeys) {
+      if (key.startsWith("INPUT_ENVKEY_")) {
+        const value = process.env[key] || "";
+
+        if (value === "" && core.getInput("fail_on_empty") === "true") {
+          throw new Error(`Empty env key found: ${key}`);
+        }
+
+        data.env_variables[key.slice(13)] = value;
+      }
+    }
+
+    // const directory = core.getInput("directory") || "";
+    // const fileName = core.getInput("file_name") || ".env";
+    // let filePath = process.env["GITHUB_WORKSPACE"] || ".";
+
+    // if (filePath === "" || filePath === "None") {
+    //   filePath = ".";
+    // }
+
+    // if (directory === "") {
+    //   filePath = path.join(filePath, fileName);
+    // } else if (directory.startsWith("/")) {
+    //   throw new Error(
+    //     "Absolute paths are not allowed. Please use a relative path."
+    //   );
+    // } else if (directory.startsWith("./")) {
+    //   filePath = path.join(filePath, directory.slice(2), fileName);
+    // } else {
+    //   filePath = path.join(filePath, directory, fileName);
+    // }
+
+    const yamlDump = yaml.dump(data);
+
+    core.debug(JSON.stringify(data));
+    core.debug(yamlDump);
+
+    //fs.writeFileSync(filePath, outFile);
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+}
+
+run();
